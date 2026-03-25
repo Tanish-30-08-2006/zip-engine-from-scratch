e801fe5744e32868aefd2342124838206c14fede
Tanish <tanishsanghavi2@gmail.com>
2026-03-25 17:55:21 +0530

    feat: implement manual_encoder.py for direct bit-assignment testing

---
+++ 01-information-theory/1-2-fixed-vs-variable-length-encoding/manual_encoder.py
@@ -0,0 +1,87 @@
+import os
+import collections
+
+def run_automatic_encoder():
+    print("--- The Compression Lab: Automatic Variable-Length Encoder ---")
+
+    #--------------------------READING DATA-----------------------#
+
+    # File path 
+    base_dir = os.path.dirname(__file__)
+    file_name = input(F"Enter file name(from data folder) to fetch data from : (eg: complex.txt) :  ")
+    file_path = os.path.join(base_dir,"..","data",file_name)
+
+    # Does the file exists 
+    if not os.path.exists(file_path):
+        print(f"Error: The file '{filename}' was not found.")
+        return
+    
+    with open(file_path,'r',encoding='utf-8') as file:
+        file_content = file.read()
+    
+    if not file_content:
+        print("Empty input! Try 'BEEP BOOP' or 'BANANA'")
+        return
+    
+    #--------------------------FREQUENCY----------------------------#
+
+    counts_per_char      = collections.Counter(file_content)
+    total_chars          = len(file_content)
+
+    # sort most frequent ---> least frequent
+    # most frequent will be first in counts_per_char
+    sorted_chars = counts_per_char.most_common()
+
+
+
+    #-------------------------CODE ASSIGNMENT--------------------------#
+
+    custom_dictionary = {}
+    current_prefix = ""
+
+    print("\n[Step 1] Generating Custom Dictionary:")
+    print(f"{'Char':<10} | {'Count':<7} | {'New Binary Code'}")
+    print("-" * 35)
+
+    for i , (char,count) in enumerate(sorted_chars):
+        
+        if i == (len(sorted_chars) -1):
+            # if it is very last (rarest) charzcter we dont need to write trailing 0's
+            code = "1"*i if i>0 else "0"
+        else:
+            # create a string of 'i' ones followed by a '0'
+            # Example: i=0 -> '0', i=1 -> '10', i=2 -> '110'
+            code = ("1"*i) + "0"
+        
+        custom_dictionary[char] = code
+
+        display_char = f"'{char}'" if char != " " else "Space"
+        print(f"{display_char:<10} | {count:<7} | {code}")
+
+    #--------------------------ENCODING WHOLE FILE CONTENT------------------------------#
+
+    # We replace every character in the original text with our new code
+    encoded_stream = ""
+    
+    for char in file_content:
+        encoded_stream = encoded_stream + custom_dictionary[char]
+    
+
+    #----------------------------------COMPARISON-----------------------------------#
+
+    ascii_size        = (total_chars)*(8)
+    compressed_size   = len(encoded_stream)
+    size_saved        = (1 - (compressed_size / ascii_size)) * 100
+
+    print("\n The Resulting Bit-Stream:")
+    print(f"Binary: {encoded_stream}") 
+    
+    print("\n[Step 3] Final Efficiency Audit:")
+    print(f"Original Size (ASCII 8-bit): {ascii_size} bits")
+    print(f"New Compressed Size:         {compressed_size} bits")
+    print(f"Space Saved:                 {size_saved:.2f}%")
+
+if __name__ == "__main__":
+    run_automatic_encoder()
+    
+    
